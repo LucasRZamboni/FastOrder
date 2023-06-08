3a553e706cfab132f3730a68d0193cdc92385147
LucasRZamboni <lucasrzamboni@hotmail.com>
2023-06-08 16:13:30 -0300

    alteração em teste.js

--- js/teste.js
+++ js/teste.js
@@ -1,4 +1,4 @@
 const urlApp = window.location.href.replace(/\/[^\/]*$/, '');
 function login() {
-    window.location.href = `${urlApp}/home.html`;
+    window.location.href = `${urlApp}/menu.html`;
 }
